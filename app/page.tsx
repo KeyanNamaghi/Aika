0e3aa3cc345983630329df31d2c0a11126996154
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2024-11-30 20:42:01 +0000

    use supabase template

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
-import { getCurrentSession } from "@/lib/server/session";
-import { redirect } from "next/navigation";
-import { Logout } from "@/components/logout";
-import { globalGETRateLimit } from "@/lib/server/request";
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
-export default async function Page() {
-  if (!globalGETRateLimit()) {
-    return "Too many requests";
-  }
-  const { user } = await getCurrentSession();
-  if (user === null) {
-    return redirect("/login");
-  }
+export default async function Index() {
   return (
     <>
-      <h1>Hi, {user.name}!</h1>
-      <img src={user.picture} height="100px" width="100px" alt="profile" />
-      <Logout />
+      <Hero />
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      </main>
     </>
   );
 }
