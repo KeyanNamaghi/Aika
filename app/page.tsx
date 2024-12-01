7f6d1689f7c2203bef99a69b96e4f0a8a0950858
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2024-12-01 20:22:41 +0000

    cleaning up a little more

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 import Hero from '@/components/hero'
-import ConnectSupabaseSteps from '@/components/tutorial/connect-supabase-steps'
-import SignUpUserSteps from '@/components/tutorial/sign-up-user-steps'
-import { hasEnvVars } from '@/utils/supabase/check-env-vars'
 
 export default async function Index() {
   return (
@@ -9,7 +6,7 @@ export default async function Index() {
       <Hero />
       <main className='flex flex-1 flex-col gap-6 px-4'>
         <h2 className='mb-4 text-xl font-medium'>Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        Probably should add some content here.
       </main>
     </>
   )
--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -1,7 +1,5 @@
 import { signOutAction } from '@/actions/auth'
-import { hasEnvVars } from '@/utils/supabase/check-env-vars'
 import Link from 'next/link'
-import { Badge } from './ui/badge'
 import { Button } from './ui/button'
 import { createClient } from '@/utils/supabase/server'
 
@@ -12,39 +10,6 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (!hasEnvVars) {
-    return (
-      <>
-        <div className='flex items-center gap-4'>
-          <div>
-            <Badge variant={'default'} className='pointer-events-none font-normal'>
-              Please update .env.local file with anon key and url
-            </Badge>
-          </div>
-          <div className='flex gap-2'>
-            <Button
-              asChild
-              size='sm'
-              variant={'outline'}
-              disabled
-              className='pointer-events-none cursor-none opacity-75'
-            >
-              <Link href='/sign-in'>Sign in</Link>
-            </Button>
-            <Button
-              asChild
-              size='sm'
-              variant={'default'}
-              disabled
-              className='pointer-events-none cursor-none opacity-75'
-            >
-              <Link href='/sign-up'>Sign up</Link>
-            </Button>
-          </div>
-        </div>
-      </>
-    )
-  }
   return user ? (
     <div className='flex items-center gap-4'>
       Hey, {user.email}!
