0a8d5360a29669bc5668313c6b50453a9956f7eb
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2025-04-11 17:42:13 +0100

    add texture background and enhance border styles in globals.css; update layout structure in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en' className={GeistSans.className} suppressHydrationWarning>
       <body className='bg-background text-foreground'>
+        <div className='texture' />
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
           <main className='flex min-h-screen flex-col items-center'>
             <div className='flex w-full flex-1 flex-col items-center gap-20'>
-              <nav className='flex h-16 w-full justify-center border-b border-b-foreground/10'>
+              <nav className='border-b-foreground/10 flex h-16 w-full justify-center border-b'>
                 <div className='flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm'>
                   <div className='flex items-center gap-5 font-semibold'>
                     <Link href={'/'}>Aika</Link>
@@ -31,7 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 </div>
               </nav>
               {children}
-              <footer className='mb-4 mt-16 grid w-full gap-4'>
+              <footer className='mt-16 mb-4 grid w-full gap-4'>
                 <Separator />
                 <div className='flex h-5 items-center justify-center space-x-4 text-xs'>
                   <p>
