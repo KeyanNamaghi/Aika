457f265a0a800b40a4a0f90bb3a30e3e45baf1b5
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2025-06-19 15:38:29 +0100

    fix: remove debug log from middleware function

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
-  console.log('🚨' + request.nextUrl.origin)
-
   return await updateSession(request)
 }
 
